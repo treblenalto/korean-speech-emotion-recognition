50bd42cf965f397c27ce7ee841648ab8356a3c70
Taehee-K <taehee107275@gmail.com>
2021-07-15 21:48:05 +0900

    Delete missing audio speech

--- preprocess/label.py
+++ preprocess/label.py
@@ -6,7 +6,7 @@ df = pd.read_csv('../data/5차_10011.csv',encoding='CP949')
 columns = ['1번 감정', '2번 감정', '3번 감정', '4번 감정', '5번 감정']
 df_em = df[columns]
 
-# 가장 많이 나왔던 라벨 값 
+# most common emotion label
 label = []
 for i in range(len(df_em)):
   emotions = Counter(df_em.iloc[i])
@@ -16,14 +16,19 @@ speech = df['발화문']
 most_common = {'speech': speech, 'emotion' : label}
 most_common = pd.DataFrame(most_common)
 
-# 감정 >> 인덱스 값으로 변환하기
-cat_to_id = {'Angry': 0,
-            'Disgust': 1,
-            'Fear': 2,
-            'Happiness': 3,
-            'Neutral': 4,
-            'Sadness': 5,
-            'Surprise': 6}
+# # convert emotion >> index values 
+# cat_to_id = {'Angry': 0,
+#             'Disgust': 1,
+#             'Fear': 2,
+#             'Happiness': 3,
+#             'Neutral': 4,
+#             'Sadness': 5,
+#             'Surprise': 6}
 
-most_common['emotion'].map(cat_to_id)
+# most_common['emotion'].map(cat_to_id)
+
+# delete speech not mapped to audio files
+err = [1107,1283,1997,3086,3386,7851,8324]    # missing audio file index
+most_common = most_common.drop(err)
+
 most_common.to_csv("../data/most_common.csv", index = False)
